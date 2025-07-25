89a633a448fedea951ff7b3360175b9e28b666ed
charles cunningham <c.a.cunningham6@gmail.com>
2025-07-25 16:00:46 +0200

    initial doom config

---
+++ custom.el
@@ -0,0 +1,14 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("f053f92735d6d238461da8512b9c071a5ce3b9d972501f7a5e6682a90bf29725" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
