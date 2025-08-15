7af26c91423af757cab231d08d976fdf22c777ac
charles cunningham <c.a.cunningham6@gmail.com>
2025-08-15 20:40:52 +0200

    emacs everywhere

--- init.el
+++ init.el
@@ -201,7 +201,7 @@
        :app
        ;;calendar
        ;;emms
-       ;;everywhere        ; *leave* Emacs!? You must be joking
+       everywhere        ; *leave* Emacs!? You must be joking
        (claude-code +vterm)
        ;;irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
