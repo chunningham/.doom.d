df93dd42be940ce4b6e3d755fba47182c2a13f26
charles cunningham <c.a.cunningham6@gmail.com>
2025-08-19 18:43:42 +0200

    pin claude code ide

--- modules/app/claude-code/packages.el
+++ modules/app/claude-code/packages.el
@@ -3,7 +3,9 @@
 
 ;; Main package
 (package! claude-code-ide
-  :recipe (:host github :repo "manzaltu/claude-code-ide.el"))
+  :recipe (:host github
+           :repo "manzaltu/claude-code-ide.el")
+  :pin "853440e3d8cebfef49ba9da5e2991422b8c778f4")
 
 (when (and (not (modulep! :term vterm)) (modulep! +vterm))
   (package! vterm :pin "056ad74653704bc353d8ec8ab52ac75267b7d373"))
