62bd5ec3ff735bdf2fdb25808d93e61d1864c0db
Charles Cunningham <charlescunningham@Charless-MacBook-Pro.local>
2025-10-20 13:01:54 -0300

    some improvements

--- modules/app/eaf/packages.el
+++ modules/app/eaf/packages.el
@@ -1,6 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; app/eaf/packages.el
-;;;
+
 (package! eaf
   :recipe (:host github
            :repo "emacs-eaf/emacs-application-framework"
@@ -28,5 +28,6 @@
                       eaf-mail
                       eaf-git
                       eaf-evil
-                      eaf-markmap)
+                      eaf-markmap
+                      eaf-all-the-icons)
            :pre-build (("python" "install-eaf.py" "--install-all-apps"))))
